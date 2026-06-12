37507b3dcd9abf8af7be14fc4b78582351b13ce8
oscarcv125 <a01741965@tec.mx>
2026-06-11 19:54:27 -0600

    crear el repositorio

---
+++ dislexia/dislexiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dislexiaApp.swift
+//  dislexia
+//
+//  Created by isa chairez on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct dislexiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dislexiaTests/dislexiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  dislexiaTests.swift
+//  dislexiaTests
+//
+//  Created by isa chairez on 11/06/26.
+//
+
+import Testing
+@testable import dislexia
+
+struct dislexiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
